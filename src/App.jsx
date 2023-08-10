e2731fe2e47481ad610665f1ff7add82c609aa35
Badsha Hossain <badshab454@gmail.com>
2023-08-10 14:53:53 +0600

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 const App = () => {
   return (
     <>
-      <h2>Hello World!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-        inventore doloribus ratione impedit veniam vel quis reiciendis tenetur
-        mollitia! Vero!
-      </p>
+      <h2>Hello World</h2>
     </>
   );
 };
