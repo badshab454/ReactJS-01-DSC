c279b7794da4599cd9c5985edc8c4827c8bbe1db
Badsha Hossain <badshab454@gmail.com>
2023-08-10 14:43:13 +0600

    App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
       <h2>Hello World!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+        inventore doloribus ratione impedit veniam vel quis reiciendis tenetur
+        mollitia! Vero!
+      </p>
     </>
   );
-}
+};
 
 export default App;
