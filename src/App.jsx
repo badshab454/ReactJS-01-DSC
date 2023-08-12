a4cbd7fd38977c4fc00085b4c211ecb4787daad3
Badsha Hossain <badshab454@gmail.com>
2023-08-12 12:58:28 +0600

    common css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import "./App.css";
+
 const App = () => {
   return (
     <>
-      <h2>Hello World</h2>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Location</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <button>
+          <a href="#">Login</a>
+        </button>
+      </nav>
     </>
   );
 };
